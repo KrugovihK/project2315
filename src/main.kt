8b8653f9a04dc3c6b897f7e7f0a33eff126d77ad
Dimexz <dmnoykin@gmail.com>
2021-01-13 11:19:46 +0700

    это мой прект

---
+++ src/main.kt
@@ -0,0 +1,35 @@
+fun main()
+{
+    println("дарова")
+    println("хочешь фокус")
+    println("введи первую цифру")
+    val num1 = readLine().toString().toDouble()
+    println("введи вторую цифру")
+    val num2 = readLine().toString().toDouble()
+    println("введи  операцию")
+    val operation = readLine()
+
+    when(operation)
+    {
+        "+" -> println(num1+num2)
+        "-" -> println(num1-num2)
+        "/" -> println(num1/num2)
+        "*" -> println(num1*num2)
+        else -> println("опа короч не получилось")
+
+    }
+    println("плати податки столько сколько написанно")
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
